873ef68c30c8962e807f0a04a5587b64104ff56d
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-12 08:58:38 +0000

    use install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 # first, run this from an interactive zsh terminal session:
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+
+cat antidote_init.sh > ~/.zshrc
+cp .zsh_plugins.txt ~/
+cp .p10k.zsh ~/
