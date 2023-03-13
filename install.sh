9434b0f50761cf4a484e1cff6949263f63c66de8
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-13 03:22:27 +0000

    try again

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ done
 
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 source ${ZDOTDIR:-~}/.antidote/antidote.zsh
-antidote load
+antidote bundle <~/.zsh_plugins.txt >~/.zsh_plugins.zsh
+source ~/.zsh_plugins.zsh
