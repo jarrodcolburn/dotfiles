fcaaf22943d457d56015e157d2f7a467f9028fba
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-13 00:06:45 +0000

    delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-# first, run this from an interactive zsh terminal session:
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
-
-cat antidote_init.sh > ~/.zshrc
-cp .zsh_plugins.txt ~/
-cp .p10k.zsh ~/
