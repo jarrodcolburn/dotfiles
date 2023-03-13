4b4fef723e6dfb9a9c827ba00f8ad6a5519ca68f
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-13 02:15:05 +0000

    fix: didn't load p10k properly

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 
 # Because cp can't find dot files
-for FILE in $(ls copy) ; do
-    cp -f copy/$FILE ~/.$FILE
+for SRC in copy/* ; do
+    DEST=~/.$(basename $SRC)
+    echo "Force copying $SRC to $DEST"
+    rm -f $DEST
+    cp -f $SRC $DEST 
 done
