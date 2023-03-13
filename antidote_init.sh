da638b2ddbe5f63d5f65e89063ee959f48ca8974
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-13 00:07:49 +0000

    delete antidote

--- antidote_init.sh
+++
@@ -1,7 +0,0 @@
-# now, simply add these two lines in your ~/.zshrc
-
-# source antidote
-source ${ZDOTDIR:-~}/.antidote/antidote.zsh
-
-# initialize plugins statically with ${ZDOTDIR:-~}/.zsh_plugins.txt
-antidote load
